df141c536dc2f008ff31f3d0a95e392f4a6c677a
renzothenoob <72588010+renzothenoob@users.noreply.github.com>
2021-12-05 17:52:56 +0800

    [ADD] birthday-reminder-api

---
+++ birthday-reminder-api/src/test/java/com/bdayreminder/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdayreminder.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
