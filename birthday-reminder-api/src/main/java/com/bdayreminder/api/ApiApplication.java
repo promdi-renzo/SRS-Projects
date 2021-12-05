c6b33634c7b178128e4bed5864723e2d309b9a7d
renzothenoob <72588010+renzothenoob@users.noreply.github.com>
2021-12-05 18:13:21 +0800

    [ADD] static bday entities

--- birthday-reminder-api/src/main/java/com/bdayreminder/api/ApiApplication.java
+++ birthday-reminder-api/src/main/java/com/bdayreminder/api/ApiApplication.java
@@ -1,13 +1,35 @@
 package com.bdayreminder.api;
 
+import com.bdayreminder.api.entities.Bday;
+import com.bdayreminder.api.repository.BdayRepository;
+import com.bdayreminder.api.services.BdayService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class ApiApplication {
+public class ApiApplication  {
+
+	@Autowired
+	private BdayService service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demoData(BdayRepository repo) {
+		return args -> {
+
+			repo.save(new Bday(0, "Renzo", "Cabarios", 21, "https://picsum.photos/200"));
+			repo.save(new Bday(0, "Renzo", "Cabarios", 21, "https://picsum.photos/200"));
+			repo.save(new Bday(0, "Renzo", "Cabarios", 21, "https://picsum.photos/200"));
+			repo.save(new Bday(0, "Renzo", "Cabarios", 21, "https://picsum.photos/200"));
+			repo.save(new Bday(0, "Renzo", "Cabarios", 21, "https://picsum.photos/200"));
+
+		};
 	}
 
 }
