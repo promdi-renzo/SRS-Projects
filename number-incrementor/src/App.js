1995e846040a68672135a2f525833e4d2c1d3228
renzothenoob <72588010+renzothenoob@users.noreply.github.com>
2021-12-05 13:07:04 +0800

    Add incrementByOne function

--- number-incrementor/src/App.js
+++ number-incrementor/src/App.js
@@ -1,4 +1,11 @@
+import { useState } from "react";
+
 function App() {
+  const [number, setnumber] = useState(0);
+
+  const incrementByOne = () => {
+    setnumber(number + 1);
+  };
   return (
     <div className="container">
       <button>Number</button>
