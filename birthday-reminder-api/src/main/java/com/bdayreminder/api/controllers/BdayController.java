fb76f6cd5ae6fe08d7544a0d8f4d90ffda353c99
renzothenoob <72588010+renzothenoob@users.noreply.github.com>
2021-12-05 18:12:50 +0800

    [ADD] Bday Controller

---
+++ birthday-reminder-api/src/main/java/com/bdayreminder/api/controllers/BdayController.java
@@ -0,0 +1,26 @@
+package com.bdayreminder.api.controllers;
+
+
+import com.bdayreminder.api.entities.Bday;
+import com.bdayreminder.api.services.BdayService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping("/api")
+@RestController
+@CrossOrigin("*")
+public class BdayController {
+
+    @Autowired
+    private BdayService service;
+
+    @GetMapping("/bday")
+    public List<Bday> getAllBday(){
+        return service.getAllBday();
+    }
+}
