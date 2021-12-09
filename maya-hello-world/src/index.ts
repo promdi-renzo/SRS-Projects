a754f26d7a0937876f46e842a6765e236d4e392d
renzothenoob <72588010+renzothenoob@users.noreply.github.com>
2021-12-09 11:29:05 +0800

    [DEL] comments

--- maya-hello-world/src/index.ts
+++ maya-hello-world/src/index.ts
@@ -1,9 +1,4 @@
 import { configServer } from "@mayajs/core";
 import { AppModule } from "./app.module";
 
-configServer(3333)
-  .usePlugins([
-    // Add plugins or middlewares here
-    // i.e. CORS, body-parser, multer and etc...
-  ])
-  .bootstrapModule(AppModule);
+configServer(3333).usePlugins([]).bootstrapModule(AppModule);
