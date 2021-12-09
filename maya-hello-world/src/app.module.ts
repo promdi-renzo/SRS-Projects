71ebc5e7222a886a31eec5dbfefaa4cf546a65d7
renzothenoob <72588010+renzothenoob@users.noreply.github.com>
2021-12-09 11:28:53 +0800

    [DEL] unecessary files

--- maya-hello-world/src/app.module.ts
+++ maya-hello-world/src/app.module.ts
@@ -1,14 +1,8 @@
 import { Module } from "@mayajs/core";
-import { RouterModule } from "@mayajs/router";
-import { routes } from "./app.routing.module";
 import { AppController } from "./app.controller";
-import { MongoDbModule, MongoDbServices } from "@mayajs/mongo";
-import { mongoOptions } from "./database/mongo";
 
 @Module({
   declarations: [AppController],
-  imports: [RouterModule.forRoot(routes), MongoDbModule.forRoot(mongoOptions)],
-  providers: [MongoDbServices],
   bootstrap: AppController,
 })
 export class AppModule {}
