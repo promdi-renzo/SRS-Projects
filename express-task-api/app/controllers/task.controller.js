b33e4a4ac14247a63f81923a8076e1038588d158
renzothenoob <72588010+renzothenoob@users.noreply.github.com>
2021-12-18 15:32:53 +0800

    [MOD] implement task service in task controller

--- express-task-api/app/controllers/task.controller.js
+++ express-task-api/app/controllers/task.controller.js
@@ -1,30 +1,30 @@
-const Task = require("../models/Task");
+const taskService = require("../services/task.service");
 
 const getAllTask = async (req, res) => {
-  const tasks = await Task.find({});
+  const tasks = await taskService.getAllTask();
   res.send({ tasks });
 };
 
 const getTask = async (req, res) => {
   const { id } = req.params;
-  const task = await Task.findOne({ _id: id });
+  const task = await taskService.getAllTask(id);
   res.send({ task });
 };
 
 const addTask = async (req, res) => {
-  const task = await Task.create(req.body);
+  const task = await taskService.addTask(req.body);
   res.send({ task });
 };
 
 const updateTask = async (req, res) => {
   const { id } = req.params;
-  const task = await Task.findByIdAndUpdate({ _id: id }, req.body);
+  const task = await taskService.updateTask(id, req.body);
   res.send({ task });
 };
 
 const deleteTask = async (req, res) => {
   const { id } = req.params;
-  const task = await Task.findOneAndDelete({ _id: id });
+  const task = await taskService.deleteTask(id);
   res.send({ task });
 };
 
