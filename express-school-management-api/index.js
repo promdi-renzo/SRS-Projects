16c2f4b58846ecb7fa1a6e95497797b8b76c72a6
renzothenoob <72588010+renzothenoob@users.noreply.github.com>
2021-12-21 07:57:35 +0800

    [IMPL] assignment route

--- express-school-management-api/index.js
+++ express-school-management-api/index.js
@@ -1,16 +1,18 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-const ENV = require("./app/environment/index");
 const connectDB = require("./app/db/connect");
 const userRoute = require("./app/routes/user.route");
 const activityRoute = require("./app/routes/activity.route");
 const announcementRoute = require("./app/routes/announcement.route");
-const bodyParser = require("body-parser");
+const assignmentRoute = require("./app/routes/assignment.route");
+const ENV = require("./app/environment/index");
 
 app.use(bodyParser.json());
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/activity", activityRoute);
 app.use("/api/v1/announcement", announcementRoute);
+app.use("/api/v1/assignment", assignmentRoute);
 
 const start = () => {
   connectDB(ENV.mongo_con).then(() => {
