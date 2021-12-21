4b41af50d47798f3a4201df88fd34d8430fec9aa
renzothenoob <72588010+renzothenoob@users.noreply.github.com>
2021-12-21 08:13:14 +0800

    [ADD] grade service

---
+++ express-school-management-api/app/routes/services/grade.service.js
@@ -0,0 +1,34 @@
+const gradeModel = require("../models/grade.model");
+
+const getAllGrade = async () => {
+  const grades = await gradeModel.find();
+  return grades;
+};
+
+const getGradeById = async (id) => {
+  const grade = await gradeModel.find({ _id: id });
+  return grade;
+};
+
+const addGrade = async (body) => {
+  const grade = await gradeModel.create(body);
+  return grade;
+};
+
+const updateGrade = async (id, body) => {
+  const grade = await gradeModel.findByIdAndUpdate({ _id: id }, body);
+  return grade;
+};
+
+const deleteGradeById = async (id) => {
+  const grade = await gradeModel.findByIdAndDelete({ _id: id });
+  return grade;
+};
+
+module.exports = {
+  getAllGrade,
+  getGradeById,
+  addGrade,
+  updateGrade,
+  deleteGradeById,
+};
