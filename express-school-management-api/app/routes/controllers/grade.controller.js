3100fc30466fa50c50f89aaf878e65e276be64cf
renzothenoob <72588010+renzothenoob@users.noreply.github.com>
2021-12-21 08:13:22 +0800

    [ADD] grade controller

---
+++ express-school-management-api/app/routes/controllers/grade.controller.js
@@ -0,0 +1,37 @@
+const gradeService = require("../services/grade.service");
+
+const getAllGrade = async (req, res) => {
+  const grades = await gradeService.getAllGrade();
+  res.send({ grades });
+};
+
+const getGradeById = async (req, res) => {
+  const { id } = req.params;
+  const grade = await gradeService.getGradeById(id);
+  res.send({ grade });
+};
+
+const addGrade = async (req, res) => {
+  const grade = await gradeService.addGrade(req.body);
+  res.send({ grade });
+};
+
+const updateGrade = async (req, res) => {
+  const { id } = req.params;
+  const grade = await gradeService.updateGrade(id, req.body);
+  res.send({ grade });
+};
+
+const deleteGradeById = async (req, res) => {
+  const { id } = req.params;
+  const grade = await gradeService.deleteGradeById(id);
+  res.send({ grade });
+};
+
+module.exports = {
+  getAllGrade,
+  getGradeById,
+  addGrade,
+  updateGrade,
+  deleteGradeById,
+};
